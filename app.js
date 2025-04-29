c034234d69baf2d2ab3722591bbb383055bb671c
campregher07 <danieljunioor38@gmail.com>
2025-04-29 20:09:44 -0300

    fim da tela consulta de blocos, estilização basica e inicio da consulta de apt

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mysql = require("mysql2");
 const app = express();
+const bodyParser = require("body-parser")
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
 
 const connection = mysql.createConnection({
     host: 'localhost', //endereço do banco de dados
@@ -19,78 +24,177 @@ connection.connect(function(err){
     } console.log("Conexão ok! ")
 });
 
-app.listen(8083, function(){
-console.log("Servidor rodando na URL http://localhost:8083")
+// Consulta de Bloco
+
+app.get("/cadastroBloco", function(req, res){
+    res.render("create")
+});
+app.get("/edit/:idbloco", function(req, res){
+    const idbloco = req.params.idbloco;
+    const query = 'SELECT * FROM bloco WHERE idbloco = ?';
+
+    connection.query(query, [idbloco], (err, results) => {
+        if (err) return res.status(500).send('Erro ao buscar o bloco');
+        if (results.length === 0) return res.status(404).send('Bloco não encontrado');
+
+        res.render('edit', { bloco: results[0] });
+    });
 });
 
 app.get("/", function(req, res){
     const select = "Select * from bloco"
 
-    connection.query(select, function(err, rows){
-        if (!err){
-            res.send(`
-                <!DOCTYPE html>
-                        <html lang="pt-br">
-                        <head>
-                            <meta charset="UTF-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <title>Gerenciamento de blocos</title>
-                        </head>
-                        <body>
-                            <h1>Condomínio</h1>
-
-                            <h2>Pesquisar bloco</h2>
-                            <input type="text" name="pesquisa" placeholder="ex: bloco A" >
-
-                            <table>
-                            <tr>
-                                <th>descrição</th>
-                                <th>Qtd de apartamentos</th>
-                                <th>ações</th>
-                            </tr>
-
-                            ${rows.map(row => `
-                                <tr>
-                                    <td>${row.descricao}</td>
-                                    <td>${row.qtdApartamentos}</td>
-                                    <td><a href="/excluir/${row.idbloco}">Excluir</a>
-                                        <a href="/editar/${row.idbloco}">Editar</a></td>
-                                </tr>
-                                </tr>
-                                `).join('')}
-                        </table>
-                        <a href='/'> Voltar </a>
-    
-                        </body>
-                        </html>
-                    `)
+    connection.query(select, function(err, results){
+        if (err){
+            return res.status(500).send('Erro ao buscar dados')
         }
-    })
+
+        res.render("index", { rows: results })
+    });
 
 });
 
-app.get("/listaBlocos", function(req, res){
-    
-});
-
-app.get("/excluir/:idbloco", function(req, res){
+app.get("/delete/:idbloco", function(req, res){
     const idbloco = req.params.idbloco;
 
     const excluir = "DELETE FROM bloco where idbloco = ?" 
 
     connection.query(excluir, [idbloco], function(err, result){
         if(err){
-            console.error("Erro ao excluir o produto: ", err);
-            res.status(500).send('Erro interno ao excluir o produto.');
+            console.error("Erro ao excluir o bloco: ", err);
+            res.status(500).send('Erro interno ao excluir o bloco.');
             return;
         }
 
         
-        console.log("Produto excluido com sucesso!");
+        console.log("Bloco excluido com sucesso!");
         res.redirect('/');
-    })
+    });
 
+});
+
+app.post("/search", function(req, res){
+    const search = req.body.pesquisa
+    const query = "SELECT * FROM bloco where descricao = ?"
+
+    connection.query(query, [search], function(err, results){
+        if(err){
+            return res.status(500).send('Erro ao buscar dados')
+        }if(results.length === 0){
+            return res.status(404).send(`
+                <!DOCTYPE html>
+                <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Bloco não existentente</title>
+                    <link rel="stylesheet" href="/style.css">
+
+                </head>
+                <h1>Bloco não existente</h1>
+                <h2>Insira um bloco válido</h2>
+
+                <a href="/" >Voltar</a>
+                </html>
+                `
+            )}
+        res.render("index", { rows: results })
+    });
+
+
+});
+
+app.post("/create", function(req, res){
+    const descricao = req.body.descricao
+    const qtdApt = req.body.qtdApt
+
+    const create = "INSERT INTO bloco (descricao, qtdApartamentos) VALUES (?, ?)"
+
+    connection.query(create, [descricao, qtdApt], function(err, results){
+        if (err) {
+            console.log("Não foi possível inserir os dados:", err);
+            return res.send(`
+                <html>
+                <h1>ERRO</h1>
+                <h2>Bloco ja existente. Por favor, crie um bloco não existente!<h2>
+                <a href="/cadastroBloco">Voltar</a>
+                </html>
+                `);
+        }
+
+        console.log("Dados inseridos com sucesso:");
+        res.redirect("/");
+    });
 });
 
 
 
+app.post("/edit/:idbloco", function(req, res){
+    const idbloco = req.params.idbloco;
+    const descricao = req.body.descricao;
+    const qtdApt = req.body.qtdApt;
+
+    const update = "UPDATE bloco SET descricao = ?, qtdApartamentos = ? WHERE idbloco = ?";
+ 
+    connection.query(update, [descricao, qtdApt, idbloco], function(err, result){
+        if(!err){
+            console.log("Bloco editado com sucesso!");
+            res.redirect('/'); 
+        }else{
+            console.log("Erro ao editar o bloco ", err);
+            res.send("Erro")
+        }
+    });
+}); 
+
+// Consulta de apartamentos
+
+app.get("/apartment", function(req, res){
+    const select = "Select * from apartamento"
+
+    connection.query(select, function(err, results){
+        if (err){
+            return res.status(500).send('Erro ao buscar dados')
+        }
+
+        res.render("apartment", { rows: results })
+    });
+
+});
+
+app.post("/searchApt", function(req, res){
+    const search = req.body.pesquisa
+    const query = "SELECT * FROM apartamento where numeroApartamento = ?"
+
+    connection.query(query, [search], function(err, results){
+        if(err){
+            return res.status(500).send('Erro ao buscar dados')
+        }if(results.length === 0){
+            return res.status(404).send(`
+                <!DOCTYPE html>
+                <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Apartamento não existentente</title>
+                    <link rel="stylesheet" href="/style.css">
+
+                </head>
+                <h1>Apartamento não existente</h1>
+                <h2>Insira um apartamento válido</h2>
+
+                <a href="/apartment" >Voltar</a>
+                </html>
+                `
+    )}
+
+        res.render("apartment", { rows: results })
+    });
+
+
+});
+
+
+app.listen(8083, function(){
+    console.log("Servidor rodando na URL http://localhost:8083")
+    });
